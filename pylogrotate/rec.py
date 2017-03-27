729aae3ccae4068ce2772a6e1aa59bb01e15f127
fjkfwz <fjkfwz@gmail.com>
2017-03-28 06:54:23 +0800

    fix not delete origin file

--- pylogrotate/rec.py
+++ pylogrotate/rec.py
@@ -35,10 +35,11 @@ class Reader(object):
         hourly_dirpath_template = os.path.join(log_home, "{subdir_name}/{log_tag}-rotates/%Y/%m/%d/%H")
         hourly_dirpath_template = hourly_dirpath_template.format(log_tag=log_tag, subdir_name=subdir_name)
         dirpath = split_datetime.strftime(hourly_dirpath_template)
-        self.iter = self.load_need_to_split_file(log_tag, split_datetime, dirpath)
+        self.split_file_path = self.get_split_file_path(log_tag, split_datetime, dirpath)
+        self.iter = self.load_need_to_split_file()
 
     @classmethod
-    def load_need_to_split_file(cls, log_tag, split_datetime, dirpath):
+    def get_split_file_path(cls, log_tag, split_datetime, dirpath):
         if not os.path.exists(dirpath):
             raise OSError("HAVE NOT THIS FOLDER")
         filename_regex = "{log_tag}-(\d{{14}})\.gz".format(log_tag=log_tag)
@@ -49,12 +50,15 @@ class Reader(object):
             filename_datetime_str = re.search(filename_regex, filename).groups()[0]
             filename_datetime = datetime.datetime.strptime(filename_datetime_str, '%Y%m%d%H%M%S')
             filepath = os.path.join(dirpath, filename)
-            datetime_distance_dict[filepath] = abs((filename_datetime - split_datetime).total_seconds())
+            if truncate_hour(datetime.datetime.fromtimestamp(os.path.getctime(filepath))) == truncate_hour(split_datetime):
+                datetime_distance_dict[filepath] = abs((filename_datetime - split_datetime).total_seconds())
 
-        split_file_path = sorted(datetime_distance_dict.items(), key=itemgetter(1))[0][0]
-        if not os.path.exists(split_file_path):
+        return sorted(datetime_distance_dict.items(), key=itemgetter(1))[0][0]
+
+    def load_need_to_split_file(self):
+        if not os.path.exists(self.split_file_path):
             raise OSError("HAVE NOT THIS FILE")
-        for line in cls.read_gz_file(split_file_path):
+        for line in self.read_gz_file(self.split_file_path):
             yield line
 
     @classmethod
@@ -81,6 +85,9 @@ class Reader(object):
         next_line = self.iter.next()
         return self.extra_datetime(next_line), next_line
 
+    def __del__(self):
+        os.remove(self.split_file_path)
+
 
 def get_all_tag(log_home='/mfs/log/nginx'):
     tag_name_set = set()
@@ -90,6 +97,12 @@ def get_all_tag(log_home='/mfs/log/nginx'):
     for tag_name in tag_name_set:
         if tag_name not in ["track.im_exp.log-rotates", "track.im_clk.log-rotates"]:
             yield tag_name.replace('-rotates', '')
+
+
+def write(log_home, log_tag, rotated_time, subdir_name, log_matrix):
+    with Writer(log_home=log_home, log_tag=log_tag, rotated_time=rotated_time, subdir_name=subdir_name) as f:
+        for _log_line in log_matrix:
+            f.writelines(_log_line)
 
 
 def rec_log(split_datetime, log_tag, log_home='/mfs/log/nginx'):
@@ -104,15 +117,19 @@ def rec_log(split_datetime, log_tag, log_home='/mfs/log/nginx'):
             if not start_time:
                 start_time = dt
             if dt - start_time > datetime.timedelta(minutes=5):
-                with Writer(log_home=log_home, log_tag=log_tag, rotated_time=dt, subdir_name=subdir_name) as f:
-                    for _log_line in log_matrix:
-                        f.writelines(_log_line)
+                write(log_home, log_tag, dt, subdir_name, log_matrix)
                 start_time = None
                 log_matrix = list()
+        rotated_time = start_time + datetime.timedelta(minutes=5)
+        write(log_home, log_tag, rotated_time, subdir_name, log_matrix)
 
 
 def truncate_time(d):
     return datetime.datetime.strptime(d, '%Y-%m-%d %H:%M:%S')
+
+
+def truncate_hour(d):
+    return datetime.datetime.strptime(d, '%Y-%m-%d %H')
 
 
 def main():
